36be10cfb117a0ccf70eb56b74e0e667a6d1963d
Katia Krut <katiakrut448@gmail.com>
2022-09-20 14:48:30 +0300

    .

--- my-first-haskell-project/src/Main.hs
+++ my-first-haskell-project/src/Main.hs
@@ -1,15 +1,9 @@
 module Main where
 
 
-import Control.Exception
-
 main :: IO ()
 main = do
   putStrLn "hello world"
-
-addOne :: Integer -> Integer
-addOne x = x + 1
-
 
 
 checkRectangle :: Float -> Float -> Float -> Float -> Bool
@@ -17,12 +11,16 @@ checkRectangle side1 side2 side3 side4 =
   side1 == side2 && side3 == side4 || side1 == side3 && side2 == side4 || side1 == side4 && side3 == side2
 
 
+checkRectangleArr :: [Float] -> Bool
+checkRectangleArr arr
+  | null arr = error "No values!"
+  | length arr /= 4 = error "Must be transmitted 4 values"
+  | otherwise = arr !! 0 == arr !! 1 && arr !! 2 == arr !! 3 || arr !! 0 == arr !! 2 && arr !! 1 == arr !! 3 || arr !! 0 == arr !! 3 && arr !! 2 == arr !! 1
 
---checkTupleRectangle :: (Float -> Float -> Float -> Float) -> Bool
---checkTupleRectangle (side1, side2, side3, side4) =
---  side1 == side2 && side3 == side4 || side1 == side3 && side2 == side4 || side1 == side4 && side3 == side2
-
-checkRectangleArr :: [Float] -> IO()
-checkRectangleArr arr = do
-  if null arr
-    then  error "Oops!" else putStrLn $ show arr
+---  | otherwise = do
+--    let x = arr !! 0
+--    let y = arr !! 1
+--    let z = arr !! 2
+--    let i = arr !! 3;
+--    checkRectangle x y z i
+---

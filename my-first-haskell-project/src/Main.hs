bf17e72f5eb727977fc399adf143a4f71938672a
Katia Krut <katiakrut448@gmail.com>
2022-09-21 08:17:45 +0300

    .

--- my-first-haskell-project/src/Main.hs
+++ my-first-haskell-project/src/Main.hs
@@ -6,6 +6,37 @@ main = do
   putStrLn "hello world"
 
 
+
+-- Variant 12 ----------------------------------------------------------------------------------------------------------
+
+{-
+[[(Bool,String)],[Double]]
+[[(True, "kek")], [3.6]]
+-}
+
+
+checkPoints :: Float -> Float -> Float -> Float -> Bool
+checkPoints x1 y1 x2 y2 =
+  x2 - x1 == y2 - y1
+
+checkPointsTuple :: ((Float, Float), (Float, Float)) -> Bool
+checkPointsTuple tuple = do
+     let fstPoint = fst tuple
+     let sndPoint = snd tuple
+     fst sndPoint - fst fstPoint == snd
+
+
+
+
+-- Variant 14 ----------------------------------------------------------------------------------------------------------
+
+
+{-
+[([Double],(Bool,Char),Integer)]
+[([5.6], (True, "m"), 4)]
+-}
+
+
 checkRectangle :: Float -> Float -> Float -> Float -> Bool
 checkRectangle side1 side2 side3 side4 =
   side1 == side2 && side3 == side4 || side1 == side3 && side2 == side4 || side1 == side4 && side3 == side2
@@ -23,3 +54,31 @@ checkRectangleArr arr
 --    let z = arr !! 2
 --    let i = arr !! 3;
 --    checkRectangle x y z i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--checkPointsWithSquare :: Float -> Float -> Float -> Float -> Float -> Float -> Float -> Float-> Bool
+--checkPoints x1 y1 x2 y2 sx1 sy1 sx2 sy2  =
+--  x2 - x1 == y2 - y1
+
+--checkPointsTupleWithSquare :: ((Float, Float), (Float, Float), (Float, Float), (Float, Float)) -> Bool
+--checkPointsTuple tuple = do
+--     let fstPoint = fst tuple
+--     let sndPoint = snd tuple
+--     fst sndPoint - fst fstPoint == snd sndPoint - snd fstPoint

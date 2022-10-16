7b60b302322d8938ed2ad449c266f5666d0c62d4
Katia Krut <katiakrut448@gmail.com>
2022-10-16 16:45:27 +0300

    .

--- Project1/src/Main.hs
+++ Project1/src/Main.hs
@@ -23,24 +23,49 @@ main = do
 
 -- 1.12 ----------------------------------------------------------------------------------------------------------------
 
-checkType el
-   | show (typeOf el) == "Char" = 1
-   | show (typeOf el) == "[Char]" = 0
-   | otherwise = -1
-
-
 insertN n el list
   | n == 0 || length list < n = list
   | list == [] = []
   | otherwise = take n list ++ [el] ++ insertN n el (drop n list)
 
 
-addItem str item = concat (map (\x -> [x] ++ [item]) str)
 
+
+--getNumber = do putStrLn "Enter the number:"
+--               number <- readLn
+--               case findposition number box of
+--                 Just n  -> -- Do whatever
+--                 Nothing -> putStrLn "Please try again." >> getNumber
+
+
+--checkN x list n el
+--   | elemIndex x list `mod` n == 0 = [el]
+--   | otherwise = []
+
+
+--insertN2 0 el list = list
+--insertN2 n el list = concat (map (\x -> [x] ++ checkN x list n el) list)
+
+
+--insertN2 n el list
+-- | n <= 0 || length list < n = list
+-- | otherwise = concat (map (\x -> [x] ++ [elemIndex x list]) list)
+
+-- | otherwise = concat (map (\x -> [x] ++ if elemIndex x list `mod` n == 0 then [el] else []) list)
+-- | otherwise = concat (map (\x -> [x] ++ checkN x list n el) list)
+
+--checkN x list n el
+--   | case ind x of list `mod` n == 0 = [el]
+--   | otherwise = []
+
+ind x list = findIndex (==x) list
+
+addItem str item = concat (map (\x -> [x] ++ [item]) str)
 
 --insertAtN n y xs = intercalate [y] . groups n $ xs
 --  where
 --    groups n xs = takeWhile (not.null) . unfoldr (Just . splitAt n) $ xs
+
 
 
 -- 2.12 ----------------------------------------------------------------------------------------------------------------
@@ -54,3 +79,32 @@ getPrime list = find isPrime list
 getPrime2 :: [Integer] -> Maybe Integer
 getPrime2 list = find isPrime2 list
 
+
+--
+--checkN x
+--   | x /= 0 && x `mod` 3 == 0 = "X"
+--   | otherwise = []
+
+--checkN :: Integer -> Integer -> a -> [a]
+checkN x n el
+   | snd x `mod` n == 0 = [el]
+   | otherwise = []
+
+--mp str = concat (map (\x -> [x] ++ "X") str)
+--mp str = concat (map (\x -> [fst x] ++ show [snd x]) str)
+--mp str = concat (map (\x -> [fst x] ++ checkN snd x 3 "X") str)
+
+--indexed :: [a] -> [(a, Integer)]
+indexed ls = zip ls [1..length ls]
+
+generateEl x = [fst x] ++ (checkN x) 3 'X'
+
+mp ls = concat (map (\x -> generateEl x) ls)
+
+m ls = mp (indexed ls)
+
+
+-- | "abcdf"
+-- | mp [('a',1),('b',2),('c',3),('d',4),('e',5),('f',6)]
+-- | "abcXdefX"
+-- | "abcXdefX"

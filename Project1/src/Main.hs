b6f46761e09f2c5aba13fc6a45b4caaec91f52b5
Katia Krut <katiakrut448@gmail.com>
2022-10-17 10:55:01 +0300

    .

--- Project1/src/Main.hs
+++ Project1/src/Main.hs
@@ -1,6 +1,5 @@
 module Main (main) where
 
-import Data.Typeable
 import Data.List
 import Data.Numbers.Primes
 
@@ -23,15 +22,67 @@ main = do
 
 -- 1.12 ----------------------------------------------------------------------------------------------------------------
 
+-- | Фунція для вставлення значення у список через кожні n елементiв
+-- >> insertN 3 0 [3..12]
+-- > [3,4,5,0,6,7,8,0,9,10,11,0,12]
+--
+-- >> insertN 2 'z' "abcdefg"
+-- > "abzcdzefzg"
+insertN :: Int -> a -> [a] -> [a]
 insertN n el list
   | n == 0 || length list < n = list
-  | list == [] = []
+  | null list = []
   | otherwise = take n list ++ [el] ++ insertN n el (drop n list)
 
 
-ind x list = findIndex (==x) list
+-- | Функція для індексування списку елементів
+-- >> indexed "abcdefg"
+-- > [('a',1),('b',2),('c',3),('d',4),('e',5),('f',6),('g',7)]
+--
+-- >> indexed [1..9]
+-- > [(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9)]
+indexed :: [a] -> [(a, Int)]
+indexed ls = zip ls [1..length ls]
 
-addItem str item = concat (map (\x -> [x] ++ [item]) str)
+
+-- | Функція для перевірки якого елементу додавати
+-- >> checkN ('a', 4) 2 '0'
+-- > "0"
+checkN :: (a, Int) -> Int -> a -> [a]
+checkN x n el
+   | snd x `mod` n == 0 = [el]
+   | otherwise = []
+
+
+-- | Функція для конкатенації елементу списку з необхідним елементом
+-- >> generateEl ('a', 8) 2 'X'
+-- > "aX"
+--
+-- >> (4, 8) 2 0
+-- > [4,0]
+generateEl :: (a, Int) -> Int -> a -> [a]
+generateEl x n el = [fst x] ++ (checkN x) n el
+
+
+-- |
+-- >> mapping [('a',1),('b',2),('c',3),('d',4),('e',5),('f',6),('g',7)] 3 'X'
+-- > "abcXdefXg
+--
+-- >> mapping [(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9)] 3 0
+-- > [1,2,3,0,4,5,6,0,7,8,9,0]
+mapping :: [(a, Int)] -> Int -> a -> [a]
+mapping ls n el = concat (map (\x -> generateEl x n el) ls)
+
+
+-- | Фунція для вставлення значення у список через кожні n елементiв
+-- >> m "abcdef" 3 'X'
+-- > "abcXdefX"
+--
+-- >> m [1..9] 3 0
+-- > [1,2,3,0,4,5,6,0,7,8,9,0]
+m :: [a] -> Int -> a -> [a]
+m ls n el = mapping (indexed ls) n el
+
 
 -- 2.12 ----------------------------------------------------------------------------------------------------------------
 
@@ -44,17 +95,10 @@ getPrime list = find isPrime list
 getPrime2 :: [Integer] -> Maybe Integer
 getPrime2 list = find isPrime2 list
 
-checkN x n el
-   | snd x `mod` n == 0 = [el]
-   | otherwise = []
 
 
-indexed ls = zip ls [1..length ls]
-generateEl x n el = [fst x] ++ (checkN x) n el
-mp ls n el = concat (map (\x -> generateEl x n el) ls)
-m ls n el = mp (indexed ls) n el
--- m "abcdef" 3 'X'
--- m [1..9] 3 0
+
+
 
 
 
